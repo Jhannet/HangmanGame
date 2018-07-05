a3b08040acb69c54906e181dc11599d1270a533e
Administrator <Administrator@DEV25JHJAWIN>
2018-07-05 13:06:00 -0400

    function for readFile, problems with the syncronizer

--- src/game.js
+++ src/game.js
@@ -1,14 +1,20 @@
 const fs = require('fs')
-
+const games = [];
 class Game {
-	constructor (id) {
+	constructor (id, word) {
 		this.id = id;
-		this.hint = '_ _ _ _ A';
+		this.hint = word;//'_ _ _ _ A';
 		this.leftAttempts = 5;
 	}
-	static create() {
+	static create(word) {
 		return new Promise((resolve, reject) => {
-			let game = new Game(1);
+			let game = new Game(1, word);
+			// games = readFile('./assets/saved-games.json',games);
+			fs.readFile('./assets/saved-games.json','utf-8', function (err, data) {
+			  if (err) console.log(err)//throw err;
+			  // gamesBuffer.push(data);
+			  console.log(data);
+			});
 			if (game != null) {
 				fs.writeFile('./assets/saved-games.json', JSON.stringify(game), function (err) {
 				  if (err){
@@ -21,6 +27,15 @@ class Game {
 
 			return reject(game);		
 		})
+	}
+
+	readFile(url, gamesBuffer) {
+		fs.readFile(url,'utf-8', function (err, data) {
+		  if (err) throw err;
+		  // gamesBuffer.push(data);
+		  console.log(data);
+		});
+		return gamesBuffer;
 	}
 	toString(){
 		return `"${this.id}": {\n"id": ${this.id},\n"hint": "${this.hint}",\n"leftAttempts": ${this.leftAttempts}\n},\n`;
